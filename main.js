81f9653fe3f2cb4b48a4b7269eddea1c9f32e713
adele <adele.r87@gmail.com>
2021-02-18 14:48:05 +0100

    modifiche

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
    el: "#app",
    data: {
      bgImg: 'img/header-image-homepage.jpg',
+     linkHeader:['home' , 'about', 'departments', 'articles'],
+     logo:'img/medical_logo_1x_light.png',
 
 
 
