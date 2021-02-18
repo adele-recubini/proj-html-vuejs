e3c852de08c40a9c423fd5f34f2993c5d69ab7f6
adele <adele.r87@gmail.com>
2021-02-18 17:37:00 +0100

    modifiche

--- main.js
+++ main.js
@@ -2,8 +2,47 @@ var app = new Vue({
    el: "#app",
    data: {
      bgImg: 'img/header-image-homepage.jpg',
-     linkHeader:['home' , 'about', 'departments', 'articles'],
      logo:'img/medical_logo_1x_light.png',
+     linkHeader:['HOME' , 'ABOUT', 'DEPARTMENTS', 'ARTICLES'],
+     iconSocial:[
+       {
+         name: "twitter-square",
+         group: "fab",
+         prefix: "fa-",
+       }
+       {
+         name: "youtube",
+         group: "fab",
+         prefix: "fa-",
+       }
+       {
+         name: "facebook-square",
+         group: "fab",
+         prefix: "fa-",
+       }
+     ],
+
+    iconPosition:[
+       {
+         name: "phone",
+         group: "far",
+         prefix: "fa-",
+         contact: "121 King Street, London United Kingdom"
+       }
+       {
+         name: "map-marker-alt",
+         group: "far",
+         prefix: "fa-",
+         phone:"1.800.458.556"
+
+       }
+     ],
+
+     medicalDepartment:[ "Cardiology" , "pediatrics" , "Diabetes Care" , "Pre-natel-care", "Ultrasound Echocardiogram" ]
+
+
+
+
 
 
 
@@ -12,6 +51,11 @@ var app = new Vue({
    }
 
 
-
+   // <i class="fab fa-twitter-square"></i>
+   // <i class="fab fa-youtube"></i>
+   // <i class="fab fa-facebook-square"></i>
+   // <i class="fab fa-facebook-square"></i>
+   // <i class="far fa-phone"></i>
+   // <i class="far fa-map-marker-alt"></i>
 
  })
